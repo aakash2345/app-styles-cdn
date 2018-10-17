02013f832bc7967917ef6a0d3bb145920edfe213
Aakash Nethikunta <aakash.af666@gmail.com>
2018-10-17 09:28:31 -0400

    global styles added

---
+++ Phc_Styles_App/Gulpfile.js
@@ -0,0 +1,52 @@
+﻿/// <binding AfterBuild='sass' />
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+// other content removed
+gulp.task("sass", function () {
+    return gulp.src('Styles/*.scss')
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(gulp.dest('./wwwroot/css'));
+});
+
+gulp.task("css-concat", function () {
+    return gulp.src([
+        'wwwroot/lib/bootstrap-4.0.0/dist/css/bootstrap.min.css',
+        'wwwroot/lib/font-awesome/css/font-awesome.min.css',
+        'wwwroot/lib/kendo-ui/styles/kendo.common-bootstrap.min.css',
+        'wwwroot/lib/kendo-ui/styles/kendo.bootstrap.min.css',
+        'wwwroot/css/global-styles.css'
+    ])
+        .pipe(concat('app-min-styles.css'))
+        .pipe(gulp.dest('wwwroot/css'))
+})
+
+gulp.task("js", function () {
+    return gulp.src(['wwwroot/lib/jquery/dist/jquery.js',
+        'wwwroot/lib/popper/popper.js',
+        'wwwroot/lib/bootstrap/dist/js/bootstrap.js',
+        'wwwroot/lib/kendo-ui/js/kendo.all.min.js',
+        'wwwroot/lib/kendo-ui/js/kendo.aspnetmvc.min.js',
+        'wwwroot/lib/metisMenu/metisMenu.js',
+        'wwwroot/lib/jquery-slimscroll/jquery-slimscroll.js'
+    ])
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest('wwwroot/js'));
+})
+
+gulp.task("js-min", function () {
+    return gulp.src('wwwroot/js/*.js')
+        .pipe(uglify('app.min.js'))
+        .pipe(gulp.dest('wwwroot/js/js-min/'));
+})
+
+
+    
+    
+    
+    
+    
+    
+
+
---
+++ Phc_Styles_App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Phc_Styles.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
